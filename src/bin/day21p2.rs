e055facfd92b2884a31be659bd6684e714d9228c
jakobrs <jakobrs100@gmail.com>
2022-12-21 16:20:31 +0100

    Day 21 part 2: Refactor (it's slower now but who cares)

--- src/bin/day21p2.rs
+++ src/bin/day21p2.rs
@@ -1,4 +1,10 @@
 #![feature(test)]
+#![feature(array_zip)]
+
+use std::{
+    fmt::Display,
+    ops::{Add, Div, Mul, Sub},
+};
 
 use rustc_hash::FxHashMap;
 
@@ -14,88 +20,150 @@ fn main() {
 }
 
 fn solve(monkeys: &FxHashMap<&str, Monkey>) -> i64 {
-    fn recurse<'a>(monkeys: &FxHashMap<&'a str, Monkey<'a>>, name: &'a str) -> Val {
+    fn recurse<'a>(monkeys: &FxHashMap<&'a str, Monkey<'a>>, name: &'a str) -> Polynomial<2> {
         match monkeys[name] {
-            Monkey::Int { value } => Val::Int { value },
-            Monkey::Human => Val::Human { operations: vec![] },
+            Monkey::Polynomial(polynomial) => polynomial,
             Monkey::Operation { op, dep1, dep2 } => {
                 let dep1 = recurse(monkeys, dep1);
                 let dep2 = recurse(monkeys, dep2);
 
-                match (dep1, dep2) {
-                    (Val::Int { value: lhs }, Val::Int { value: rhs }) => Val::Int {
-                        value: op.perform(lhs, rhs),
-                    },
-                    (Val::Human { mut operations }, Val::Int { value }) => {
-                        operations.push(HumanOperation { op, rhs: value });
-                        Val::Human { operations }
-                    }
-                    (Val::Int { value }, Val::Human { mut operations }) => {
-                        match op {
-                            Operation::Plus | Operation::Times => operations.push(HumanOperation { op, rhs: value }),
-                            Operation::Minus => {
-                                operations.push(HumanOperation { op: Operation::Times, rhs: -1 });
-                                operations.push(HumanOperation { op: Operation::Plus, rhs: value });
-                            }
-                            Operation::Div => panic!("no"),
+                match op {
+                    Operation::Plus => dep1 + dep2,
+                    Operation::Minus => dep1 - dep2,
+                    Operation::Times => dep1 * dep2,
+                    Operation::Div => {
+                        if dep2.coefficients[1] == 0. {
+                            dep1 / dep2.coefficients[0]
+                        } else {
+                            panic!("no")
                         }
-                        Val::Human { operations }
                     }
-                    (Val::Human { .. }, Val::Human { .. }) => panic!("Human has multiplicity > 1"),
                 }
             }
         }
     }
 
-    let Monkey::Operation { dep1, dep2, .. } = &monkeys["root"] else { panic!("root invalid") };
-    let lhs = recurse(monkeys, dep1);
-    let rhs = recurse(monkeys, dep2);
+    let polynomial = recurse(monkeys, "root");
 
-    let (mut value, operations) = match (lhs, rhs) {
-        (Val::Int { value }, Val::Human { operations }) => (value, operations),
-        (Val::Human { operations }, Val::Int { value }) => (value, operations),
-        (Val::Human { .. }, Val::Human { .. }) => panic!("Human has multiplicity > 1"),
-        (Val::Int { .. }, Val::Int { .. }) => panic!("where human"),
-    };
-
-    for &HumanOperation { op, rhs } in operations.iter().rev() {
-        use Operation::*;
-        match op {
-            Plus => value -= rhs,
-            Minus => value += rhs,
-            Times => {
-                assert!(value % rhs == 0, "fractional temporaries are unimplemented");
-                value /= rhs;
-            }
-            Div => value *= rhs,
-        }
-    }
-
-    value
+    (-polynomial.coefficients[0] / polynomial.coefficients[1]).round() as i64
 }
 
 enum Monkey<'a> {
-    Int {
-        value: i64,
-    },
+    Polynomial(Polynomial<2>),
     Operation {
         op: Operation,
         dep1: &'a str,
         dep2: &'a str,
     },
-    Human,
 }
 
-#[derive(Debug)]
-enum Val {
-    Int { value: i64 },
-    Human { operations: Vec<HumanOperation> },
+#[derive(Debug, Clone, Copy)]
+struct Polynomial<const DEGREE: usize> {
+    coefficients: [f64; DEGREE],
 }
 
-#[derive(Debug)]
-struct HumanOperation {
-    op: Operation,
-    rhs: i64,
+impl<const N: usize> Display for Polynomial<N> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut first = true;
+        for (i, &n) in self.coefficients.iter().enumerate() {
+            if n == 0. {
+                continue;
+            }
+            if first {
+                first = false;
+                if n < 0. {
+                    f.write_str("-")?;
+                }
+            } else {
+                if n < 0. {
+                    f.write_str(" - ")?;
+                } else {
+                    f.write_str(" + ")?;
+                }
+            }
+            let n = n.abs();
+
+            if n == 1. {
+                match i {
+                    0 => write!(f, "1"),
+                    1 => write!(f, "x"),
+                    _ => write!(f, "x^{i}"),
+                }
+            } else {
+                match i {
+                    0 => write!(f, "{n}"),
+                    1 => write!(f, "{n} x"),
+                    _ => write!(f, "{n} x^{i}"),
+                }
+            }?;
+        }
+
+        Ok(())
+    }
+}
+
+impl<const N: usize> Polynomial<N> {
+    fn pointwise(self, rhs: Self, mut op: impl FnMut(f64, f64) -> f64) -> Self {
+        Self {
+            coefficients: self
+                .coefficients
+                .zip(rhs.coefficients)
+                .map(|(a, b)| op(a, b)),
+        }
+    }
+}
+
+impl<const N: usize> Add<Polynomial<N>> for Polynomial<N> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        self.pointwise(rhs, |a, b| a + b)
+    }
+}
+
+impl<const N: usize> Sub<Polynomial<N>> for Polynomial<N> {
+    type Output = Self;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        self.pointwise(rhs, |a, b| a - b)
+    }
+}
+
+impl Mul<Polynomial<2>> for Polynomial<2> {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        Self {
+            coefficients: [
+                self.coefficients[0] * rhs.coefficients[0],
+                self.coefficients[1] * rhs.coefficients[0]
+                    + self.coefficients[0] * rhs.coefficients[1],
+            ],
+        }
+    }
+}
+
+impl<const N: usize> Mul<f64> for Polynomial<N> {
+    type Output = Self;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        Self {
+            coefficients: self.coefficients.map(|n| n * rhs),
+        }
+    }
+}
+
+impl<const N: usize> Div<f64> for Polynomial<N> {
+    type Output = Self;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        Self {
+            coefficients: self.coefficients.map(|n| {
+                // assert!(n % rhs == 0);
+                n / rhs
+            }),
+        }
+    }
 }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
@@ -117,16 +185,6 @@ impl Operation {
             _ => None,
         }
     }
-
-    fn perform(self, lhs: i64, rhs: i64) -> i64 {
-        use Operation::*;
-        match self {
-            Plus => lhs + rhs,
-            Minus => lhs - rhs,
-            Times => lhs * rhs,
-            Div => lhs / rhs,
-        }
-    }
 }
 
 fn parse(input: &str) -> FxHashMap<&str, Monkey> {
@@ -137,12 +195,20 @@ fn parse(input: &str) -> FxHashMap<&str, Monkey> {
             (
                 name,
                 if name == "humn" {
-                    Monkey::Human
+                    Monkey::Polynomial(Polynomial {
+                        coefficients: [0., 1.],
+                    })
                 } else if line.len() < 15 {
-                    let value = line[6..].parse().unwrap();
-                    Monkey::Int { value }
+                    let value: f64 = line[6..].parse().unwrap();
+                    Monkey::Polynomial(Polynomial {
+                        coefficients: [value, 0.],
+                    })
                 } else {
-                    let op = Operation::from_str(&line[11..12]).unwrap();
+                    let op = if name == "root" {
+                        Operation::Minus
+                    } else {
+                        Operation::from_str(&line[11..12]).unwrap()
+                    };
                     let dep1 = &line[6..10];
                     let dep2 = &line[13..];
                     Monkey::Operation { op, dep1, dep2 }
